21ec33983747c8d21d2e2d60b0b307a19ec4a60d
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-19 01:35:29 -0700

    Eliminate Index from state

--- src/UKanren.hs
+++ src/UKanren.hs
@@ -4,46 +4,44 @@ import Control.Monad (mzero)
 import qualified Data.Map as M
 import Data.Monoid ((<>))
 
-data Term = Var VLabel Index
+data Term = Var VLabel
           | Atom String
           | Pair Term Term
           deriving (Eq, Ord)
 
 instance Show Term where
   show t = case t of
-    Var v _  -> show v
+    Var v    -> show v
     Atom a   -> '\'' : a
     Pair l r -> "(" ++ show l ++ ", " ++ show r ++ ")"
 
 type VLabel = String
-type Index = Int
 type Stream = [State]
-type State = (Subs, Index)
-type Subs = M.Map Term Term
+type State = M.Map Term Term
 type Goal = State -> Stream
 
-var :: VLabel -> Index -> Term
+var :: VLabel -> Term
 var = Var
 
 emptyStream :: Stream
 emptyStream = []
 
 emptyState :: State
-emptyState = (M.empty, 0)
+emptyState = M.empty
 
-walk :: Term -> Subs -> Term
+walk :: Term -> State -> Term
 walk v@Var{} s = maybe v (`walk` s) $ M.lookup v s
 walk t       _ = t
 
-extend :: Term -> Term -> Subs -> Subs
+extend :: Term -> Term -> State -> State
 extend = M.insert
 
 (===) :: Term -> Term -> Goal
-(===) u v (s, c) = case unify u v s of
+(===) u v s = case unify u v s of
   Nothing -> emptyStream
-  Just s' -> return (s', c)
+  Just s' -> return s'
 
-unify :: Term -> Term -> Subs -> Maybe Subs
+unify :: Term -> Term -> State -> Maybe State
 unify u' v' s = go (walk u' s) (walk v' s)
   where go u@Var{} v@Var{} | u == v  = return s
         go u@Var{} v                 = return $ extend u v s
@@ -54,7 +52,7 @@ unify u' v' s = go (walk u' s) (walk v' s)
                                          else mzero
 
 fresh :: VLabel -> (Term -> Goal) -> State -> Stream
-fresh v f (s, c) = f (var v c) (s, succ c)
+fresh v f = f (var v)
 
 disj :: Goal -> Goal -> Goal
 disj g1 g2 sc = g1 sc <> g2 sc
@@ -64,7 +62,7 @@ conj g1 g2 sc = g1 sc >>= g2
 
 aORb :: Goal
 aORb = conj (fresh "a" $ \a -> a === Atom "a")
-            (fresh "b" $ \b -> disj (b === Var "a" 0) (b === Atom "6"))
+            (fresh "b" $ \b -> disj (b === Var "a") (b === Atom "6"))
 
 main :: IO ()
 main = print $ aORb emptyState
