b7164bf7c14cccfa39c42d1fc34f7bfe79ae51fb
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-19 10:13:51 -0700

    Add delayed streams

--- src/UKanren.hs
+++ src/UKanren.hs
@@ -1,7 +1,7 @@
 module UKanren where
 
+import           Control.Monad (MonadPlus, mzero, mplus)
 import qualified Data.Map as M
-import           Data.Monoid ((<>))
 import qualified Data.Set as S
 
 data Term = Var VLabel
@@ -15,8 +15,91 @@ instance Show Term where
     Atom a   -> '\'' : a
     Pair l r -> "(" ++ show l ++ ", " ++ show r ++ ")"
 
+data DStream a = Empty
+               | Delay (DStream a)
+               | a ::: DStream a
+               deriving (Show)
+
+instance Monad DStream where
+  return = (::: Empty)
+  Empty     >>= _ = Empty
+  (Delay s) >>= f = Delay $ s >>= f
+  (a ::: s) >>= f = f a `mplus` (s >>= f)
+
+instance MonadPlus DStream where
+  mzero               = Empty
+  mplus s1      Empty = s1
+  mplus Empty      s2 = s2
+  mplus (Delay s1) s2 = Delay $ mplus s2 s1
+  mplus (s ::: s1) s2 = s ::: mplus s2 s1
+
+-- return a >>= k = k a
+--
+--     a ::: Empty >>= k
+--     = k a `mplus` (Empty >>= f)
+--     = k a `mplus` Empty
+--     = k a
+--     QOD
+--
+-----------------------------------------------------------------------------
+-- m >>= return = m
+-----------------------------------------------------------------------------
+--
+-- Empty >>= (::: Empty)
+-- = Empty
+--
+-- Delay s >>= (::: Empty)
+-- = Delay $ s >>= (::: Empty)
+-- ** if s /= Delay    => Delay $ s >>= return      = Delay $ s = Delay s -- QOD
+--                                (assume it holds)
+--
+-- ** if s == Delay s' => Delay $ Delay $ s' >>= return -- ad inf
+--
+-- (a ::: s) >>= (::: Empty)
+-- = (a ::: Empty) `mplus` (s >>= (::: Empty))
+-- = a ::: (mplus Empty (s >>= (::: Empty)))
+-- = a ::: (s >>= (::: Empty))
+-- ** if s /= :::       => a ::: s -- QOD
+-- ** if s == a' ::: s' => a ::: (a' ::: Empty (`mplus` (s' >>= return)))
+--                      == a ::: (a' ::: (Empty `mplus` (s' >>= return)))
+--                      == a ::: a' ::: (s' >>= return) -- ad inf
+--
+-----------------------------------------------------------------------------
+-- m >>= (\x -> k x >>= h) == (m >>= k) >>= h
+-----------------------------------------------------------------------------
+--
+-- Empty >>= (\x -> k x >>= h)        == Empty
+-- (Empty >>= k) >>= h == Empty >>= h == Empty   -- QOD
+
+-- (Delay s >>= k) >>= h == (Delay $ s >>= k) >>= h
+--                       == Delay $ (s >>= k) >>= h
+-- Dealy s >>= (\x -> k x >>= h)  == Delay $ s >>= (\x -> k x >>= h)
+--            ** if s /= Delay    => Delay $ (s >>= k) >>= h
+--            ** if s == Delay s' => Delay $ Delay $ s' >>= (\x -> k x >>= h) -- ad inf
+-- 
+-- a ::: s >>= (\x -> k x >>= h) == (k a >>= h) `mplus` (s >>= (\x -> k x >>= h))
+-- (a ::: s >>= k) >>= h == (k a `mplus` (s >>= k)) >>= h
+--   ** k a == Empty => (Empty >>= h) `mplus` (s >>= (\x -> k x >>= h))
+--                   == Empty `mplus` (s ....)
+--                   == s >>= (\x -> k x >> h)
+--                   => (Empty `mplus` (s >>= k)) >>= h
+--                   == (s >>= k) >>= h -- QED
+--
+--   ** k a == Delay => (Delay b >>= h) `mplus` (s >>= ...)
+--                   == (Delay $ b >>= h) `mplus` (s >>= ...)
+--                   == Delay $ mplus (b >>= h) (s >>= ...)
+--                   => (Delay b `mplus` (s >>= k)) >>= h
+--                   == (Delay $ mplus b (s >>= k)) >>= h
+--                   == Delay $ (mplus b (s >>= k)) >>= h
+--                   == ???
+
+streamToList :: DStream a -> [a]
+streamToList Empty = []
+streamToList (Delay s) = streamToList s
+streamToList (a ::: s) = a : streamToList s
+
 type VLabel = String
-type Stream = [State]
+type Stream = DStream State
 type State = M.Map Term Term
 type Goal = State -> Stream
 
@@ -24,7 +107,7 @@ var :: VLabel -> Term
 var = Var
 
 emptyStream :: Stream
-emptyStream = []
+emptyStream = Empty
 
 emptyState :: State
 emptyState = M.empty
@@ -42,7 +125,7 @@ extend = M.insert
 (===) :: Term -> Term -> Goal
 (===) u v s = case unify u v s of
   Nothing -> emptyStream
-  Just s' -> return s'
+  Just s' -> s' ::: Empty
 
 unify :: Term -> Term -> State -> Maybe State
 unify u' v' s = go (walk u' s) (walk v' s)
@@ -58,7 +141,10 @@ fresh :: VLabel -> (Term -> Goal) -> State -> Stream
 fresh v f = f (var v)
 
 disj :: Goal -> Goal -> Goal
-disj g1 g2 sc = g1 sc <> g2 sc
+disj g1 g2 sc = g1 sc `mplus` g2 sc
+
+zzz :: Goal -> Goal
+zzz g s = Delay $ g s
 
 conj :: Goal -> Goal -> Goal
 conj g1 g2 sc = g1 sc >>= g2
@@ -67,5 +153,26 @@ aORb :: Goal
 aORb = conj (fresh "a" $ \a -> a === Atom "a")
             (fresh "b" $ \b -> disj (b === Var "a") (b === Atom "6"))
 
+inf56 :: Goal
+inf56 = fresh "x" $ \x -> disj (r5 x) (r6 x)
+  where r5 x = disj (x === Atom "5") (zzz $ r5 x)
+        r6 x = disj (x === Atom "6") (zzz $ r6 x)
+
+five :: Goal
+five = fresh "x" $ \x ->  x === Atom "5"
+fives_ :: Term -> Goal
+fives_ x = disj (x === Atom "5") (zzz $ fives_ x)
+fives :: Goal
+fives = fresh "y" fives_
+ 
+fivesRev_ :: Term -> Goal
+fivesRev_ x = disj (zzz $ fivesRev_ x) (x === Atom "5")
+fivesRev :: Goal
+fivesRev = fresh "y" fivesRev_
+
 main :: IO ()
-main = print $ aORb emptyState
+main = do
+  print $ aORb emptyState
+  print $ take 10 $ streamToList $ inf56 emptyState
+  print $ take 10 $ streamToList $ fives    emptyState
+  print $ take 10 $ streamToList $ fivesRev emptyState
